c2616e4ad224e74ef5409de046b47f46a685bf48
Anish Wagle <anish.wagle192@gmail.com>
2023-03-07 08:47:20 +0545

    changes

--- Class2/Program.cs
+++ Class2/Program.cs
@@ -5,17 +5,49 @@
         static void Main(string[] args)
         {
             //string,bool, int, float, list, dic,char
-            var list = new List<string>() { "krishna","radha"};
-            var dic = new Dictionary<string, int>();
-            dic["age"] = 20;
-            //Console.Write("x:");
-            //var x = Console.ReadLine();
-            //Console.Write("y:");
-            //var y =Console.ReadLine();
-            //var z = x.ToString();
-            var name3 = $"{dic["Husband"]} and {dic["Wife"]} are husband and wife";
-            Console.WriteLine(name3);
-            Console.WriteLine("Hello, Class!");
+            var p = "sadfa";
+            Console.Write("x:");
+            var x =int.Parse( Console.ReadLine());
+            Console.Write("y:");
+            var y =Convert.ToInt32( Console.ReadLine());
+            Console.Write("operation:");
+            var operation = Console.ReadLine();
+
+            if (operation == "/") // == != < > <= >=
+            {
+                if(x>y && y != 0) //&& - and , || or
+                {
+                    Console.WriteLine(x + y);
+                }
+                if (p == null) { }
+                
+            }
+            else if (operation == "-") 
+            {
+                Console.WriteLine(x-y);
+            }
+            else
+            {
+                Console.WriteLine("no operation found");
+            }
+
+            switch(operation) {
+                case "+":
+                    if (x > y && y != 0) //&& - and , || or
+                    {
+                        Console.WriteLine(x + y);
+                    }
+                    if (p == null) { }
+                    break;
+                case "-":
+                    Console.WriteLine(x - y);
+                    break;
+                default:
+                    Console.WriteLine("no operation found");
+                    break;
+            }
+
+         
 
         }
     }
