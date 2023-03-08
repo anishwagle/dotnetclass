af516334600ef2e05067faea3dac7e0699437607
Anish Wagle <anish.wagle192@gmail.com>
2023-03-08 08:38:02 +0545

    for, while, and do while loop examples

--- Class2/Program.cs
+++ Class2/Program.cs
@@ -4,51 +4,80 @@
     {
         static void Main(string[] args)
         {
-            //string,bool, int, float, list, dic,char
-            var p = "sadfa";
-            Console.Write("x:");
-            var x =int.Parse( Console.ReadLine());
-            Console.Write("y:");
-            var y =Convert.ToInt32( Console.ReadLine());
-            Console.Write("operation:");
-            var operation = Console.ReadLine();
 
-            if (operation == "/") // == != < > <= >=
+            //while,do while, for,foreach
+            //var i = 0;
+            //while(i<=10)
+            //{
+            //    i++;
+
+            //    if (i==5)
+            //    {
+            //        break;
+            //    }
+            //   if(i==2)
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine($"Hi{i}");
+
+            //}
+            //while(true) {
+            //    Console.Write("x:");
+            //    var x = Convert.ToInt32(Console.ReadLine());
+            //    Console.Write("y:");
+            //    var y = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Operations:+,-,*,/");
+            //    Console.WriteLine("e to end");
+            //    Console.Write("Operation:");
+            //    var z = Console.ReadLine();
+
+            //    switch(z)
+            //    {
+            //        case "+":
+            //            Console.WriteLine($"{x+y}");
+            //            break;
+            //        case "-":
+            //            Console.WriteLine($"{x - y}");
+            //            break;
+            //        case "*":
+            //            Console.WriteLine($"{x * y}");
+            //            break;
+            //        case "/":
+            //            Console.WriteLine($"{x / y}");
+            //            break;
+            //        default: 
+            //            Console.WriteLine("Enter valid operatins");
+            //            break;
+            //    }
+            //}
+
+            //do while
+            //var i = 10;
+            //do {
+            //    Console.WriteLine($"Hi{i}");
+            //    i++;
+            //} while (i<10);
+
+            //for loop
+            //for(var i = 0; i < 5; i++)
+            //{
+
+            //    for (var j = 0; j < 5; j++)
+            //    {
+
+            //        Console.WriteLine($"{i}_{j}");
+            //    }
+            //}
+            
+            for (var i =0; i< 10; i++)
             {
-                if(x>y && y != 0) //&& - and , || or
+                for(var j = 0; j < i; j++)
                 {
-                    Console.WriteLine(x + y);
+                    Console.Write("*");
                 }
-                if (p == null) { }
-                
+                Console.WriteLine();
             }
-            else if (operation == "-") 
-            {
-                Console.WriteLine(x-y);
-            }
-            else
-            {
-                Console.WriteLine("no operation found");
-            }
-
-            switch(operation) {
-                case "+":
-                    if (x > y && y != 0) //&& - and , || or
-                    {
-                        Console.WriteLine(x + y);
-                    }
-                    if (p == null) { }
-                    break;
-                case "-":
-                    Console.WriteLine(x - y);
-                    break;
-                default:
-                    Console.WriteLine("no operation found");
-                    break;
-            }
-
-         
-
         }
     }
 }
