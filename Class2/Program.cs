1adb1d55bd944fa14f9930fe9e56f82687ec7276
Anish Wagle <anish.wagle192@gmail.com>
2023-03-10 08:27:09 +0545

    List and for each loops

--- Class2/Program.cs
+++ Class2/Program.cs
@@ -4,51 +4,44 @@
     {
         static void Main(string[] args)
         {
-            //string,bool, int, float, list, dic,char
-            var p = "sadfa";
-            Console.Write("x:");
-            var x =int.Parse( Console.ReadLine());
-            Console.Write("y:");
-            var y =Convert.ToInt32( Console.ReadLine());
-            Console.Write("operation:");
-            var operation = Console.ReadLine();
 
-            if (operation == "/") // == != < > <= >=
+            //list
+            List<string> list1 = new List<string>() { "apple","banana","cat","bat","bay"};
+            var list2 = new List<string>() { "e", "f", "g","f","f" };
+            list1.Add("d");
+            list1.AddRange(list2);
+            int[] x = { 1, 2, 3, 4, 5, 6, };
+            var listx = x.ToList();
+            var arrayx = x.ToArray();
+            list1.Remove("d");
+            list1.RemoveAt(0);
+            list1.RemoveRange(0, 2);
+            list1.RemoveAll(x => x == "f");
+            Console.WriteLine(list1[2]);
+            var result = list1.Where(x => x.StartsWith("ba") || x.Contains("t")).ToList();
+            var result2 = list1.FirstOrDefault(x=>x.StartsWith("z"));
+            var startsWithZ = list1.Any(x => x.Contains("a"));
+            //var list3= list1.Union(list2);
+            var result3 =  list1.Take(2);
+            var result4 = list1.OrderBy(x=>x).ToList();
+            var result5 = list1.OrderByDescending(x=>x).ToList();
+            //var list4 = list1.Intersect(list2);
+
+            //foreach
+            foreach (var item in list1)
             {
-                if(x>y && y != 0) //&& - and , || or
+                if (item.StartsWith("ba"))
                 {
-                    Console.WriteLine(x + y);
+                    //result.Add(item);
                 }
-                if (p == null) { }
-                
-            }
-            else if (operation == "-") 
-            {
-                Console.WriteLine(x-y);
-            }
-            else
-            {
-                Console.WriteLine("no operation found");
+                Console.WriteLine(item);
             }
 
-            switch(operation) {
-                case "+":
-                    if (x > y && y != 0) //&& - and , || or
-                    {
-                        Console.WriteLine(x + y);
-                    }
-                    if (p == null) { }
-                    break;
-                case "-":
-                    Console.WriteLine(x - y);
-                    break;
-                default:
-                    Console.WriteLine("no operation found");
-                    break;
-            }
-
-         
-
+            //for loop
+            //for (var i = 0; i <= list1.Count; i++)
+            //{
+            //    Console.WriteLine(list1[i]);
+            //}
         }
     }
 }
