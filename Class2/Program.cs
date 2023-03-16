25be6de88f72b52bf6030622fac3b9414e97ae7c
Anish Wagle <anish.wagle192@gmail.com>
2023-03-16 07:27:56 +0545

    changes

--- Class2/Program.cs
+++ Class2/Program.cs
@@ -2,37 +2,62 @@
 {
     internal class Program
     {
-        
+        //10=10*9*8*..*1
         static void Main(string[] args)
         {
-         var sum=   Add(5.5f,2.3f);
-            Console.WriteLine(sum);
-            
-        }
-
-        static void WelcomeMessage(string name="Hari" )
-        {
-            Console.WriteLine("Hello "+name);
-            Console.WriteLine("Everything is great");
-        }
-
-        
-        static int Add(int a,int b)
-        {
-            Console.WriteLine("function2");
-
-            return a + b;
-        }
-        static float Add(float? a, float? b)
-        {
-            if(b== null)
+            var n = 10;
+            var factorial = 1;
+            while (n > 0)
             {
-                b = 0;
+                factorial *= n;
+                n--;
             }
-            Console.WriteLine("function1");
+            Console.WriteLine(factorial);
+            //Console.WriteLine(Factorial(10));
 
-            return a.Value + b.Value;
+
+            int previousNo = 0;
+            int currentNo = 1;
+            int tem = 0;
+            //0,1,1,2,3,5,8..
+            //Console.WriteLine($"{previousNo}");
+            //Console.WriteLine($"{currentNo}");
+            for (int i = 2; i <= 10; i++)//O(N)
+            {
+                tem = currentNo + previousNo;
+                previousNo = currentNo;
+                currentNo = tem;
+
+            }
+            Console.WriteLine($"{currentNo}");
+
+            //Console.WriteLine( Fib(10000));
+
         }
-        
+
+
+        //1*1*2*3*4*5*6*7*8*9*...
+        public static double Factorial(int number)//10
+        {
+            if (number == 0)
+                return 1;
+
+            return number * Factorial(number-1); 
+        }
+
+        ////0,1,1,2,3,5,8..
+
+        public static int Fib(int n)
+        {
+
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Fib(n - 1) + Fib(n - 2);
+            }
+        }
     }
 }
