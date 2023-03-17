c55d698e5a900d016c3934c1df01380d49a8ce2b
Anish Wagle <anish.wagle192@gmail.com>
2023-03-17 07:20:32 +0545

    classes

---
+++ Class2/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+  
+    public class Car
+    {
+       
+        public string Name { get; set; }
+        public string Color { get; set; }
+
+        public Car() {
+            Console.WriteLine("Init");
+        }
+        public Car(string name) {
+            Console.WriteLine("Init param");
+            Name = name;
+            Color = "REd";
+        }
+
+        public void SetName(string name)
+        {
+            Name= name;
+        }
+        public string GetName()
+        {
+            Console.WriteLine("GetName");
+            return Name;
+        }
+        public static void DisplayIntroOfGame()
+        {
+            Console.WriteLine("Welcome To Our Car Game");
+        }
+    }
+}
+namespace test
+{
+    public class Car
+    {
+
+    }
+}
--- Class2/Program.cs
+++ Class2/Program.cs
@@ -1,54 +1,16 @@
-﻿namespace Class2
+﻿using Cars;
+using test;
+
+namespace Class2
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            //string,bool, int, float, list, dic,char
-            var p = "sadfa";
-            Console.Write("x:");
-            var x =int.Parse( Console.ReadLine());
-            Console.Write("y:");
-            var y =Convert.ToInt32( Console.ReadLine());
-            Console.Write("operation:");
-            var operation = Console.ReadLine();
-
-            if (operation == "/") // == != < > <= >=
-            {
-                if(x>y && y != 0) //&& - and , || or
-                {
-                    Console.WriteLine(x + y);
-                }
-                if (p == null) { }
-                
-            }
-            else if (operation == "-") 
-            {
-                Console.WriteLine(x-y);
-            }
-            else
-            {
-                Console.WriteLine("no operation found");
-            }
-
-            switch(operation) {
-                case "+":
-                    if (x > y && y != 0) //&& - and , || or
-                    {
-                        Console.WriteLine(x + y);
-                    }
-                    if (p == null) { }
-                    break;
-                case "-":
-                    Console.WriteLine(x - y);
-                    break;
-                default:
-                    Console.WriteLine("no operation found");
-                    break;
-            }
-
-         
-
+            Cars.Car car1 = new Cars.Car("a") ;
+            
+          
+            Console.WriteLine(car1.GetName());
         }
     }
 }
