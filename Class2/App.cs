2a069282848a5f7b0927faa4e9384aa19452948e
suresh dangol <surace997@gmail.com>
2023-04-14 12:21:59 -0500

    Learning git

---
+++ Class2/App.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class2
+{
+    public void class App
+    {
+        Console.Writeline("I am a funky guy");
+    }
+}
